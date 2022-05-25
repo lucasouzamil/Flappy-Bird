d4e98fa2a9fe3b4f52c96b1b06a1ce289b6f9515
GustavoRDPaulino <gustavordp@al.insper.edu.br>
2022-05-25 16:03:00 -0300

    criando classe da maca

--- assests/jogo.py
+++ assests/jogo.py
@@ -1,10 +1,19 @@
 print('Hello World!')
+from email.mime import image
 import pygame
 pygame.init()
 WIDTH = 600
 HEIGHT = 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 game=True
+maca_image=pygame.image.load('').convert()
+class maca(pygame.sprite.Sprite):
+       def __init__(self,):
+            pygame.sprite.Sprite.__init__(self)
+            self.image=maca_image
+            self.rect = self.image.get_rect()
+            self.rect.x = (300,300)
+            self.rect.y =(300,300)
 while game:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
