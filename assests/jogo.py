77ea676be5eac691f6f52cfadb00f0c2c099007c
GustavoRDPaulino <gustavordp@al.insper.edu.br>
2022-05-25 15:54:21 -0300

    criando uma tela

--- assests/jogo.py
+++ assests/jogo.py
@@ -1,1 +1,12 @@
 print('Hello World!')
+import pygame
+pygame.init()
+WIDTH = 600
+HEIGHT = 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+game=True
+while game:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            game=False
+pygame.quit()
