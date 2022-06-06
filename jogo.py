bdfc1daf8d8e9a2f07169be7fbf70607f0acaed9
Lucas Lima <lucasfsl@al.insper.edu.br>
2022-06-06 19:50:27 -0300

    Criação da classe dos canos e adicionei os canos como obstaculos do jogo

--- jogo.py
+++ jogo.py
@@ -15,6 +15,12 @@ VELOCIDADE_JOGO = -5
 
 CHAO_ALTURA = 100
 
+CANO_WIDTH  = 90
+CANO_HEIGHT = 600
+CANO_GAP_inicial = 180
+CANO_GAP    = CANO_GAP_inicial
+CANO_TAMNHOMINIMO = 30
+
 #ASSETS
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -49,12 +55,71 @@ class Chao(pygame.sprite.Sprite):
         if self.rect.right <= 0:
             self.rect.left = SCREEN_WIDTH
 
+class Cano(pygame.sprite.Sprite):
+    def __init__(self, invertido):
+        pygame.sprite.Sprite.__init__(self)
 
+        self.cores = { 'verde':  pygame.transform.scale(pygame.image.load('assets/sprites/pipe-green.png').convert_alpha(), (CANO_WIDTH, CANO_HEIGHT)),
+                       'vermelho':pygame.transform.scale(pygame.image.load('assets/sprites/pipe-red.png').convert_alpha(), (CANO_WIDTH, CANO_HEIGHT))}
+
+        self.invertido = invertido
+        self.cordocano = 'verde'
+        self.image = self.cores[self.cordocano]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.left = SCREEN_HEIGHT-20
+        self.posicaox = 0
+
+        if self.invertido:
+            self.image = pygame.transform.flip(self.image,False,True)
+            self.rect.left = canonormal.rect.left
+            self.rect.bottom = canonormal.rect.top - CANO_GAP
+
+        else:
+            self.posicaox = random.randint(CANO_TAMNHOMINIMO+CANO_GAP, SCREEN_HEIGHT-CHAO_ALTURA-CANO_TAMNHOMINIMO)
+            self.rect.top = self.posicaox
+
+
+    def update(self):
+        self.image = self.cores[self.cordocano]
+        if self.invertido:
+            self.image = pygame.transform.flip(self.image,False,True)
+        self.movimentovertical()
+
+    def movimentovertical(self):
+        if self.invertido:
+            self.rect.left = canonormal.rect.left
+            self.rect.bottom = canonormal.rect.top - CANO_GAP
+        else:
+            self.rect.x += VELOCIDADE_JOGO
+            if self.rect.right <= 0:
+                self.rect.left = SCREEN_WIDTH
+                self.posicaox =  random.randint(CANO_TAMNHOMINIMO+CANO_GAP, SCREEN_HEIGHT-CHAO_ALTURA-CANO_TAMNHOMINIMO)
+            self.rect.top = self.posicaox
+
+    def jogardenovo(self):
+        if self.invertido:
+            self.rect.left = canonormal.rect.left
+            self.rect.bottom = canonormal.rect.top - CANO_GAP
+        else:
+            self.rect.left = SCREEN_HEIGHT-20
+            self.posicaox = random.randint(CANO_TAMNHOMINIMO+CANO_GAP, SCREEN_HEIGHT-CHAO_ALTURA-CANO_TAMNHOMINIMO)
+            self.rect.top = self.posicaox
 
 #DECLARAÇÃO DOS GRUPOS DAS CLASSES E DOS OBJETOS
 
+canos   = pygame.sprite.Group()
 chaos   = pygame.sprite.Group()
 all_sprites_jogando = pygame.sprite.Group()
+
+canonormal = Cano(False)
+canoinvert = Cano(True)
+
+canos.add(canonormal)
+canos.add(canoinvert)
+
+all_sprites_jogando.add(canonormal)
+all_sprites_jogando.add(canoinvert)
 
 for i in range(2): 
     if i == 0:
